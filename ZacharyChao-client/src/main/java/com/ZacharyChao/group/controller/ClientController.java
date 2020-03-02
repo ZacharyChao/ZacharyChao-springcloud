c6d9cfbb348c751c4e2c99ff64a50323864f2b26
ZacharyChao <41775247+ZacharyChao@users.noreply.github.com>
2020-03-02 23:08:00 +0800

    2020-03-02

--- ZacharyChao-client/src/main/java/com/ZacharyChao/group/controller/ClientController.java
+++ ZacharyChao-client/src/main/java/com/ZacharyChao/group/controller/ClientController.java
@@ -1,11 +1,13 @@
 package com.ZacharyChao.group.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClientController {
 	@GetMapping("/msg")
+	@ResponseBody
 	public String msg() {
 		return "I'm client message";
 	}
